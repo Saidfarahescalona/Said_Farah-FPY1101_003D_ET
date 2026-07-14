41bbc448e240243507cd53019278b5c297850ed3
Said Farah <sa.farah@gmail.com>
2026-07-14 08:53:38 -0400

    Adición de los diccionarios

---
+++ Said_Farah_003D.py
@@ -0,0 +1,17 @@
+planes = {
+'F001': ['Plan Básico', 'mensual', 1, False, False, 'libre'],
+'F002': ['Plan Full', 'mensual', 1, True, True, 'libre'],
+'F003': ['Plan Estudiante', 'trimestral', 3, False, True,
+'tarde'],
+'F004': ['Plan Senior', 'trimestral', 3, True, False, 'mañana'],
+'F005': ['Plan Anual Pro', 'anual', 12, True, True, 'libre'],
+'F006': ['Plan Nocturno', 'mensual', 1, False, True, 'noche'],
+}
+inscripciones = {
+'F001': [14990, 30],
+'F002': [22990, 10],
+'F003': [39990, 0],
+'F004': [35990, 6],
+'F005': [159990, 2],
+'F006': [18990, 15],
+}

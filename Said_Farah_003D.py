46c110c7fbb3168cadcbba3ac73ba992cdd11ced
Said Farah <sa.farah@gmail.com>
2026-07-14 10:56:13 -0400

    Adición opción 4 (no fui capaz de terminar el comando para añadir al diccionario asi que no funciona)

--- Said_Farah_003D.py
+++ Said_Farah_003D.py
@@ -30,8 +30,8 @@ def validacion_string(msg):
         texto = (input(msg))
         texto = texto.strip()
         texto = texto.lower()
-        if len(texto) <= 2:
-            print("ERROR: ¡El texto no puede estár practicamente vacío!")
+        if len(texto) == 0:
+            print("ERROR: ¡El texto no puede estár vacío!")
         else:
             return texto
 def cupos_tipo(tipo):
@@ -65,7 +65,62 @@ def actualizar_precio(codigo, nuevo_precio):
         return True
     else:
         return False
-
+def validador_código(codigo):
+    if codigo in planes:
+        return False
+    elif len(codigo) == 0:
+        return False
+    else:
+        return True
+def validador_nombre(nombre):
+    if len(nombre) == 0:
+        return False
+    else:
+        return True
+def validador_tipo(tipo):
+    if tipo == "mensual":
+        return True
+    elif tipo == "trimestral":
+        return True
+    elif tipo == "anual":
+        return True
+    else:
+        return False
+def validador_duración(duracion):
+    if duracion <= 0:
+        return False
+    else:
+        return True
+def validador_acceso_piscina(piscina):
+    if piscina == "s":
+        return True
+    elif piscina == "n":
+        return False
+def validador_incluye_clases(clases):
+    if clases == "s":
+        return True
+    elif clases == "n":
+        return False
+def validador_horario(horario):
+    if len(horario) == 0:
+        return False
+    else:
+        return True
+def validador_precio(precio):
+    if precio <= 0:
+        return False
+    else:
+        return True
+def validador_cupos(cupos):
+    if duracion < 0:
+        return False
+    else:
+        return True
+def agregar_plan(codigo, nombre, tipo, duracion, piscina, clases, horario, precio, cupos):
+    lista_planes = [nombre, tipo, duracion, piscina, clases, horario]
+    lista_inscripciones = [precio, cupos]
+    planes.append(codigo:lista_planes)
+    inscripciones.append(codigo:lista_inscripciones)
 while True:
     print("""
 ========== MENÚ PRINCIPAL ==========
@@ -114,7 +169,90 @@ while True:
             elif eleccion == "n":
                 break
     elif opcion == 4:
-        print("DEBUG")
+        while True:
+            codigo = input("Ingrese código del plan: ")
+            codigo = codigo.strip()
+            codigo = codigo.upper()
+            validador = validador_código(codigo)
+            if validador == False:
+                print("ERROR: ¡El código no puede estar en blanco ni tampoco estár ya existente!")
+                continue
+            elif validador == True:
+                nombre = input("Ingrese nombre del plan: ")
+                nombre = nombre.strip()
+                nombre = nombre.capitalize()
+                validador = validador_nombre(nombre)
+                if validador == False:
+                    print("ERROR: ¡El nombre no puede estár vacío!")
+                    continue
+                elif validador == True:
+                    tipo = input("Ingrese tipo (mensual/trimestral/anual): ")
+                    tipo = tipo.strip()
+                    tipo = tipo.lower()
+                    validador = validador_tipo(tipo)
+                    if validador == False:
+                        print("ERROR: ¡Debe ser exactamente 'mensual', 'trimestral' o 'anual'!")
+                        continue
+                    elif validador == True:
+                        try:
+                            duracion = int(input("Ingrese duración (meses): "))
+                            validador = validador_duración(duracion)
+                            if validador == False:
+                                print("ERROR: ¡La duración ha de ser mayor que 0!")
+                            elif validador == True:
+                                piscina = input("¿Incluye acceso a piscina? (s/n): ")
+                                picina = piscina.strip()
+                                piscina = piscina.lower()
+                                validador = validador_acceso_piscina(piscina)
+                                if validador == False:
+                                    piscina = validador
+                                elif validador == True:
+                                    piscina = validador
+                                    clases = input("¿Incluye clases grupales? (s/n): ")
+                                    clases = clases.strip()
+                                    clases = clases.lower()
+                                    validador = validador_incluye_clases(clases)
+                                    if validador == False:
+                                        clases = validador
+                                    elif validador == True:
+                                        clases = validador
+                                        horario = input("Ingrese horario: ")
+                                        horario = horario.strip()
+                                        horario = horario.lower()
+                                        validador = validador_horario(horario)
+                                        if validador == False:
+                                            print("ERROR: ¡El horario no puede estar vacío!")
+                                            continue
+                                        elif validador == True:
+                                            try:
+                                                precio = int(input("Ingrese precio: "))
+                                                validador = validador_precio(precio)
+                                                if validador == False:
+                                                    print("ERROR: ¡El precio tiene que ser mayor a 0!")
+                                                    continue
+                                                elif validador == True:
+                                                    try:
+                                                        cupos = int(input("Ingrese cupos: "))
+                                                        validador = validador_cupos(cupos)
+                                                        if validador == False:
+                                                            print("ERROR: ¡Los cupos tienen que ser mayores o igual a 0!")
+                                                            continue
+                                                        elif validador == True:
+                                                            agregar_plan(codigo, nombre, tipo, duracion, piscina, clases, horario, precio, cupos)
+                                                            break
+                                                    except ValueError:
+                                                        print("ERROR: ¡El valor ha de ser un numero entero!")
+                                                        continue
+                                            except ValueError:
+                                                print("ERROR: ¡El valor ha de ser un numero entero!")
+                                                continue
+                        except ValueError:
+                            print("ERROR: ¡El valor ha de ser un numero entero!")
+                            continue        
+        if validador == True:
+            print("Plan agregado")
+        elif validador == False:
+            print("Codigo ya existente")
     elif opcion == 5:
         print("DEBUG")
     elif opcion == 6:
